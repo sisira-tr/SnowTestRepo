f66777118971bcc16f2e3ba18d3a33394a00af5d
sisira-tr <Sisira.Raghunandanan@Triconinfotech.com>
2026-02-16 16:37:16 +0530

    Refactor

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import snowflake.snowpark as snowpark
+from snowflake.snowpark.functions import col
 
-def main(session: snowpark.Session): 
-    print("Hello")
+def main(session: snowpark.Session):
+    tableName = 'information_schema.packages'
+    dataframe = session.table(tableName).filter(col("language") == 'python')
+
+    dataframe.show()
+
+    return dataframe
     
